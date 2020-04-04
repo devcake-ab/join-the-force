a15b0d763ed331867cba0250708538e287366353
georgiosApo <georgios.apostolidis92@gmail.com>
2020-04-04 02:14:34 +0200

    default FBconfig

--- src/config/FBconfig.js
+++ src/config/FBconfig.js
@@ -3,13 +3,13 @@ import 'firebase/firestore'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA3Xgeq7qCJtiK0caik5Gmsa69gNRzg6Wo",
-    authDomain: "join-the-force-2b92e.firebaseapp.com",
-    databaseURL: "https://join-the-force-2b92e.firebaseio.com",
-    projectId: "join-the-force-2b92e",
-    storageBucket: "join-the-force-2b92e.appspot.com",
-    messagingSenderId: "492411876511",
-    appId: "1:492411876511:web:287a61ceeaadffbb4cdeaa"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "j",
+    messagingSenderId: "",
+    appId: ""
 }
 
 const app = firebase.initializeApp(firebaseConfig);
