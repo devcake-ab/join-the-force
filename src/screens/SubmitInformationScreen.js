6b0d0ed5cbcb879a47843786f3dfdbb7fbfaca55
William Lennartsson <william.j.lennartsson@gmail.com>
2020-04-05 16:54:07 +0200

    Thanks for submitting component + navigation back to home

--- src/screens/SubmitInformationScreen.js
+++ src/screens/SubmitInformationScreen.js
@@ -1,22 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button'
 
 import SubmissionForm from '../components/submissionForm'
 import { firestore } from '../config/FBconfig'
+import history from '../navigation/history'
 
 export default function SubmitInformationPage() {
+    const [hasSubmitted, setHasSubmitted] = useState(false)
 
     const handleOnSubmit = (submission) => {
         console.log(submission)
         // TODO: add creates a unique ID. Connect ID to a BankID sign
-        firestore.collection("submissions").add( Object.assign({}, submission) )
+    firestore.collection("submissions").add(Object.assign({}, submission))
         .then(res => res.id)
-        .then(documentId => console.log("Här har vi ett ID.", documentId))
+        .then(documentId => {
+            console.log("Här har vi ett ID.", documentId)
+            setHasSubmitted(true)
+        })
         .catch(err => console.log("Error adding document", err)) // TODO: Update UI
     }
 
+    if (!hasSubmitted) {
+        return (
+            <ThanksForSubmitting />
+        )
+    }
     return (
         // header
+        // { hasSubmitted && <thanksForSubmitting /> }
+
         <SubmissionForm onSubmit={handleOnSubmit} />
         // footer
+    )
+}
+
+const ThanksForSubmitting = () => {
+    return (
+        <div>
+            <h2>Thanks for submitting</h2>
+            <Button className="mr-3" variant="secondary" onClick={() => history.push('/home')}>Back to home</Button>
+        </div>
     )
 }
